10761d1e80d0e90e2596908f4e179ffc2cd647e0
NayanMely23 <nayan.mely@wsabronx.org>
2024-05-22 20:52:52 -0400

    first commit

---
+++ src/main/java/runner/MyRunner.java
@@ -0,0 +1,18 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		 features  = "src/test/resources/Features", 
+		    glue = "stepDefinition", // Corrected package name
+		    plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"}	
+		
+		
+		)
+
+
+
+public class MyRunner extends AbstractTestNGCucumberTests  {
+
+}
